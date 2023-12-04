548e7197707b203083c95967caea6efe6a5874e1
Mikhail Vyrodov <m_vyrodov@mail.ru>
2023-12-04 23:15:38 +0300

    Add solution and tests for task 10

--- project/automata_operations/recursive_finite_automaton.py
+++ project/automata_operations/recursive_finite_automaton.py
@@ -1,10 +1,14 @@
 from pathlib import Path
 from typing import Dict, Union
 
+from scipy.sparse import lil_array, lil_matrix
 from pyformlang.cfg import CFG
-from pyformlang.finite_automaton import FiniteAutomaton
+from pyformlang.finite_automaton import FiniteAutomaton, State
 
 from project.context_free_grammars.extended_context_free_grammar import ECFG
+from project.automata_operations.automaton_boolean_decomposition import (
+    make_state_indices_dict,
+)
 
 
 class RFA:
@@ -24,8 +28,9 @@ class RFA:
 
     @classmethod
     def rfa_from_ecfg(
-        cls, ecfg: Union[ECFG, CFG, str, Path], start_symbol: str
+        cls, ecfg: Union[ECFG, CFG, str, Path], start_symbol: str = "S"
     ) -> "RFA":
+        real_ecfg = None
         if isinstance(ecfg, CFG):
             real_ecfg = ECFG.ecfg_from_cfg_class(ecfg)
         elif isinstance(ecfg, str):
@@ -50,3 +55,42 @@ class RFA:
     def minimize(self):
         for nonterminal, automaton in self.automata.items():
             self.automata[nonterminal] = automaton.minimize()
+
+    def get_states(self):
+        states, start_states, final_states = set(), set(), set()
+        for nonterminal, automaton in self.automata.items():
+            for state in automaton.states:
+                new_state = State((nonterminal, state.value))
+                states.add(new_state)
+
+                if state in automaton.start_states:
+                    start_states.add(new_state)
+                if state in automaton.final_states:
+                    final_states.add(new_state)
+
+        return states, start_states, final_states
+
+    def get_binary_decomposition(self):
+        boolean_decomposition = {}
+        states, start_states, final_states = self.get_states()
+
+        states_dict = make_state_indices_dict(states)
+
+        for nonterminal, automaton in self.automata.items():
+            for start, transitions in automaton.to_dict().items():
+                for label, end_states in transitions.items():
+                    if not isinstance(end_states, set):
+                        end_states = {end_states}
+
+                    if label not in boolean_decomposition:
+                        boolean_decomposition[label] = lil_array(
+                            (len(states), len(states)), dtype=bool
+                        )
+
+                    for state in end_states:
+                        first_index = states_dict[State((nonterminal, start.value))]
+                        second_index = states_dict[State((nonterminal, state.value))]
+
+                        boolean_decomposition[label][first_index, second_index] = True
+
+        return boolean_decomposition
